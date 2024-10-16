eb4ef6de7073824d4f89e25db8a9cdaf1c9dc5be
ouyangca <51312196+ouyangca@users.noreply.github.com>
2024-10-16 04:38:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
+    board.IO33, # type: ignore
+    board.IO34, # type: ignore
+    board.IO48,
+    board.IO35,
+    board.IO36, # type: ignore
+    board.IO37,
+    board.IO38,
+    board.IO39
     # do the rest...
 ]
 
@@ -22,14 +30,28 @@ for led in leds:
     led.direction = Direction.OUTPUT
 
 # main loop
+history_level = 0
+
 while True:
     volume = microphone.value
 
     print(volume)
+    if volume >= 24000:
+        level = int((volume-24000)/1000)
+        print(level)
+    else:
+        level = 0 
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    if level < history_level:
+        level = history_level - 1
 
+    for i in range(11):
+        if i <= level:
+            leds[i].value = True
+        else:
+            leds[i].value = False
+
+    history_level = level
     sleep(1)
 
     # instead of blinking,
